59af9433b8246f9343ee0666fee5b05d4668e4d0
David Beal <david.beal@akretion.com>
2022-05-02 12:43:04 +0200

    FIX python version in setup

--- odoo/setup.py
+++ odoo/setup.py
@@ -1,28 +1,26 @@
-# -*- coding: utf-8 -*-
-
 from setuptools import setup, find_packages
 
-with open('VERSION') as fd:
+with open("VERSION") as fd:
     version = fd.read().strip()
 
 setup(
     name="my-project-name",
     version=version,
     description="project description",
-    license='GNU Affero General Public License v3 or later (AGPLv3+)',
+    license="GNU Affero General Public License v3 or later (AGPLv3+)",
     author="Author...",
     author_email="email...",
-    url="url...",
-    packages=['songs'] + ['songs.%s' % p for p in find_packages('./songs')],
+    url="https://akretion.com",
+    packages=["songs"] + ["songs.%s" % p for p in find_packages("./songs")],
     include_package_data=True,
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved',
-        'License :: OSI Approved :: '
-        'GNU Affero General Public License v3 or later (AGPLv3+)',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: Implementation :: CPython',
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved",
+        "License :: OSI Approved :: "
+        "GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
     ],
 )
