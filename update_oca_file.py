b3fbbf5cefc13f622219dbd77e8f595976baa049
Sébastien BEAU <sebastien.beau@akretion.com>
2023-02-26 11:54:54 +0100

    Remove unwanted oca conf

--- update_oca_file.py
+++ update_oca_file.py
@@ -15,9 +15,38 @@ FILES = [
 
 base_url = "https://raw.githubusercontent.com/OCA/oca-addons-repo-template/master/src/"
 
+
+REPLACE_BLOCKS = {
+    ".pre-commit-config.yaml.jinja": [
+        (b"""  - repo: https://github.com/acsone/setuptools-odoo
+    rev: {{ repo_rev.setuptools_odoo }}
+    hooks:
+      - id: setuptools-odoo-make-default
+      {%- if generate_requirements_txt %}
+      - id: setuptools-odoo-get-requirements
+        args:
+          - --output
+          - requirements.txt
+          - --header
+          - "# generated from manifests external_dependencies"
+      {%- endif %}""", b""),
+    ]
+}
+
+def update_content(filename, content):
+    if filename in REPLACE_BLOCKS:
+        for before, after in REPLACE_BLOCKS[filename]:
+            if not before in content:
+                raise Exception(
+                    f"The following block is missing in {filename}:\n {before}"
+                    )
+            content = content.replace(before, after)
+    return content
+
 for filename in FILES:
     url = urllib.parse.urljoin(base_url, urllib.parse.quote(filename))
     res = requests.get(url)
     if res.status_code != 200:
         raise Exception(f"Fail to get file {url} response: {res.content}")
-    open(f"src/{filename}", 'wb').write(res.content)
+    content = update_content(filename, res.content)
+    open(f"src/{filename}", 'wb').write(content)
